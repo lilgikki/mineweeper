088397cf1f3389e80c99f67c450b56824c88b6e6
Ekaterina Kostyleva <71183287+lilgikki@users.noreply.github.com>
2023-03-14 01:09:55 +0300

    fix modules (#2)

--- src/components/Cell/index.tsx
+++ src/components/Cell/index.tsx
@@ -1,26 +1,26 @@
 import React, { useState } from "react";
 import styles from './Cell.module.scss'
 
-const CELL__STATE = {
-  closed: 'closed',
-  opened: 'opened',
-  flag: 'flag',
-  quest: 'quest',
-  bomb: 'bomb',
-  bombAct: 'bombAct',
-  bombFlag: 'bombFlag',
-  one: 'cell__1',
-  two: 'cell__2',
-  three: 'cell__3',
-  four: 'cell__4',
-  five: 'cell__5',
-  six: 'cell__6',
-  seven: 'cell__7',
-  eight: 'cell__8'
+enum CellNames {
+  closed = 'closed',
+  opened = 'opened',
+  flag = 'flag',
+  quest = 'quest',
+  bomb = 'bomb',
+  bombAct ='bombAct',
+  bombFlag = 'bombFlag',
+  one = 'cell__1',
+  two = 'cell__2',
+  three = 'cell__3',
+  four = 'cell__4',
+  five = 'cell__5',
+  six = 'cell__6',
+  seven = 'cell__7',
+  eight = 'cell__8'
 }
 
 const Cell:React.FC = () => {
-  const [cellState, setCellState] = useState(CELL__STATE.closed);
+  const [cellState, setCellState] = useState(CellNames.closed);
 
   return (
     <button className={styles[cellState]}>
